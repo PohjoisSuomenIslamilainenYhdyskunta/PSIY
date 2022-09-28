1d0ca78686b9b9930c05ca45b024c7a593698b4c
FuzelahamedShaik <fuzelahamed1999@gmail.com>
2022-09-28 18:15:50 +0300

    structed gallery banner

--- components/Gallery.js
+++ components/Gallery.js
@@ -3,16 +3,16 @@ import { Carousel, CarouselItem } from 'react-bootstrap'
 
 export default function Gallery(props) {
     return (
-        <div id="gallery" className="bg-white">
-            <h2 className="text-2xl font-extrabold text-gray-900 sm:text-3xl lg:px-7">Gallery</h2>
+        <div id="gallery" className="mx-auto py-5 px-4 max-w-7xl sm:px-6 lg:px-8">
+            <h2 className="text-2xl font-extrabold text-gray-900 sm:text-4xl">Gallery</h2>
                 <Carousel>
                 <Carousel.Item>
                     <img 
                         className='d-block w-100'
                         style={{
                             height: 500,
-                            paddingLeft: 200,
-                            paddingRight: 200,
+                            paddingLeft: 300,
+                            paddingRight: 300,
                             paddingBottom: 20,
                             paddingTop: 20
                         }}
@@ -21,7 +21,6 @@ export default function Gallery(props) {
                     />
                     <Carousel.Caption>
                         <h3>{props.data.gallery.header_1}</h3>
-                        <p>{props.data.gallery.des_1}</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -29,8 +28,8 @@ export default function Gallery(props) {
                         className='d-block w-100'
                         style={{
                             height: 500,
-                            paddingLeft: 200,
-                            paddingRight: 200,
+                            paddingLeft: 300,
+                            paddingRight: 300,
                             paddingBottom: 20,
                             paddingTop: 20
                         }}
@@ -39,7 +38,6 @@ export default function Gallery(props) {
                     />
                     <Carousel.Caption>
                         <h3>{props.data.gallery.header_2}</h3>
-                        <p>{props.data.gallery.des_2}</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -47,8 +45,8 @@ export default function Gallery(props) {
                         className='d-block w-100'
                         style={{
                             height: 500,
-                            paddingLeft: 200,
-                            paddingRight: 200,
+                            paddingLeft: 300,
+                            paddingRight: 300,
                             paddingBottom: 20,
                             paddingTop: 20
                         }}
@@ -57,7 +55,6 @@ export default function Gallery(props) {
                     />
                     <Carousel.Caption>
                         <h3>{props.data.gallery.header_3}</h3>
-                        <p>{props.data.gallery.des_3}</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
