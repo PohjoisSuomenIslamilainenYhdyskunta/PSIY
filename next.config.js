6f18376ca8216eb5f8478301471f0392cf539194
Syed Jawad Akhtar <syedjawadakhtar@gmail.com>
2023-06-29 23:59:08 +0300

    Initial commit for translation of the website

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     domains: ['github.com'],
   },
 }
-
+const { i18n } = require('next-i18next')
 const withYAML = require('next-yaml')
 module.exports = withYAML(module.exports)
+module.exports = { i18n}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { appWithTranslation } from 'next-i18next';
+import { NextI18NextProvider } from 'next-i18next';
+import { i18n } from '../next-i18next.config';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextI18NextProvider
+      // pass i18n instance to NextI18NextProvider
+      i18n={i18n}
+      // other NextI18NextProvider props if needed
+    >
+      <Component {...pageProps} />
+    </NextI18NextProvider>
+  );
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
