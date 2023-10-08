843026a04e58c956f2bd3a9a11dda83d6d1b9e30
Jawad <syedjawadakhtar@gmail.com>
2023-10-09 00:50:46 +0300

    Update _app.js to fix import errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import '../styles/globals.css'
 import { appWithTranslation } from 'next-i18next';
-import { NextI18NextProvider } from 'next-i18next';
+import { I18nextProvider } from 'react-i18next';
 import { i18n } from '../next-i18next.config';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextI18NextProvider
+    <I18nextProvider
       // pass i18n instance to NextI18NextProvider
       i18n={i18n}
       // other NextI18NextProvider props if needed
     >
       <Component {...pageProps} />
-    </NextI18NextProvider>
+    </I18nextProvider>
   );
 }
 
