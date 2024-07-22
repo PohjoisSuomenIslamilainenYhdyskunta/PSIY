ad1e1f416e321016c7eb99d7ba45c529728b8284
Jawad <syedjawadakhtar@gmail.com>
2024-07-23 01:45:55 +0300

    SEO optimization

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,44 @@ function Home({ data }) {
   return (
     <>
       <Head>
-         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
-         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
-        <title>{data.name} - Masjid Website</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="icon" href="/imgs/PSIY-circular.png"></link>
+
+        {/* SEO Meta Tags */}
+        <meta name="description" content="Welcome to the official website of Pohjois Suomen Islamilainen Yhdyskunta (PSIY), the Islamic Society of Northern Finland. Find information about the local Oulu masjid, our activities, events, and community services." />
+        <meta name="keywords" content="Pohjois Suomen Islamilainen Yhdyskunta, PSIY, Islamic Society of Northern Finland, Oulu mosque, Oulun moskeija, Suomen moskeija, Finland mosque, Islamic community Oulu, Islamic events Oulu, Islamic services Finland" />
+        <meta name="author" content="Pohjois Suomen Islamilainen Yhdyskunta" />
+        <meta name="robots" content="index, follow" />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Pohjois Suomen Islamilainen Yhdyskunta (PSIY) - Masjid Website" />
+        <meta property="og:description" content="Welcome to the official website of Pohjois Suomen Islamilainen Yhdyskunta (PSIY), the Islamic Society of Northern Finland. Find information about our activities, events, and community services." />
+        <meta property="og:image" content="/imgs/PSIY-circular.png" />
+        <meta property="og:url" content="https://psiy.fi" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Pohjois Suomen Islamilainen Yhdyskunta (PSIY) - Masjid Website" />
+        <meta name="twitter:description" content="Welcome to the official website of Pohjois Suomen Islamilainen Yhdyskunta (PSIY), the Islamic Society of Northern Finland. Find information about our activities, events, and community services." />
+        <meta name="twitter:image" content="/imgs/PSIY-circular.png" />
+
+        {/* Canonical Tag */}
+        <link rel="canonical" href="https://psiy.fi" />
+
+        {/* Language Tag */}
+        <meta httpEquiv="Content-Language" content="en" />
+
+        {/* Preload Important Resources */}
+        <link rel="preload" href="/_next/static/css/23676ba13117b421.css" as="style" />
+        <link rel="preload" href="/_next/static/css/703b48b595b9bf2c.css" as="style" />
+        <link rel="stylesheet" href="/_next/static/css/23676ba13117b421.css" data-n-g="" />
+        <link rel="stylesheet" href="/_next/static/css/703b48b595b9bf2c.css" data-n-p="" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
+        <title>{data.name} - Oulu Masjid</title>
         <GoogleAnalytics />
       </Head>
       <MosqueBrandingCSS data={data} />
@@ -36,7 +71,7 @@ function Home({ data }) {
 }
 
 Home.getInitialProps = async ({ req }) => {
-  const data = mosques[ req.headers.host ] ?? mosques[ "default" ]
+  const data = mosques[req.headers.host] ?? mosques["default"]
   return { data: data }
 }
 
